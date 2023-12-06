6c37440ece79414a22b490a72d2ed433c56404e4
François Martineau <ffrancoismmartineau@gmail.com>
2023-12-06 13:08:32 -0500

    Only one instance, argument logic outside server class

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: francoma <francoma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 14:13:44 by francoma          #+#    #+#             */
-/*   Updated: 2023/12/05 14:11:13 by francoma         ###   ########.fr       */
+/*   Updated: 2023/12/06 13:08:02 by francoma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,43 @@
 #include <unistd.h>
 #include "Server.hpp"
 
+struct Args
+{
+    int port;
+    std::string password;
+};
+
+static Args getArgs(int argc, char *argv[])
+{
+    if (argc != 3)
+        throw std::invalid_argument("Error: wrong number of arguments");
+    Args args;
+    std::stringstream ss(argv[1]);
+    ss >> args.port;
+    if (ss.fail() || !ss.eof() || args.port < 0 || args.port > 65535)
+        throw std::invalid_argument("Error: wrong port number");
+    ss.str(argv[2]);
+    ss.clear();
+    ss >> args.password;
+    if (ss.fail() || !ss.eof())
+        throw std::invalid_argument("Error: wrong password");
+    return args;
+}
+
 int main(int argc, char *argv[])
 {
     //std::cout << "Process ID (PID): " << getpid() << std::endl;
     try
     {
-        Server s(argc, argv);
+        Args args = getArgs(argc, argv);
+        Server& s = Server::getInstance(args.port, args.password);
         s.run();
     }
+    catch (const std::invalid_argument &e)
+    {
+        std::cerr << e.what() << std::endl << USAGE << std::endl;
+        return 1;
+    }    
     catch (std::exception const& e)
     {
         std::cerr << "ça chié dans pelle!" << std::endl
